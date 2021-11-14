52ed9e574a63514d7ec7d318b67540f8472f2d2c
anshgs <anshsharma@gmail.com>
2021-11-14 17:03:04 -0600

    separated player from walls and extended collision to multiple walls

--- src/main.cc
+++ src/main.cc
@@ -16,22 +16,35 @@ using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-float vertices[] = {
+
+float playerCoords[] = {
         0.5f, 0.5f, 0.0f, // left  
-         0.6f, 0.5f, 0.0f, // right 
+        0.6f, 0.5f, 0.0f, // right 
         0.6f, 0.6f, 0.0f, // right  
-         0.5f,  0.6f, 0.0f, // top  
-         0.1f, 0.1f, 0.0f,
+        0.5f,  0.6f, 0.0f, // top  
+};
+
+float vertices[] = {
+        0.1f, 0.1f, 0.0f,
         0.3f, 0.1f, 0.0f,
         0.3f, 0.3f, 0.0f,
         0.1f, 0.3f, 0.0f,
-    }; 
+        0.4f, -0.2f, 0.0f,
+        0.5f, -0.2f, 0.0f,
+        0.5f, 0.0f, 0.0f,
+        0.4f, 0.0f, 0.0f,
+    };
 
 float winV[] = {
     -0.1f, -0.1f, 0.0f,
     0.0f, -0.1f, 0.0f,
     0.0f, 0.0f, 0.0f, 
     -0.1f, 0.0f, 0.0f,
+};
+
+unsigned int playerInd[] = {
+    0, 1, 3,
+    1, 2, 3,
 };
 
 unsigned int winIn[] = {
@@ -169,13 +182,37 @@ int main()
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         
-    unsigned int EBO[2];
-    glGenBuffers(2, EBO);
-    unsigned int VBO[2], VAO[2];
+    unsigned int EBO[3];
+    glGenBuffers(3, EBO);
+    unsigned int VBO[3], VAO[3];
 
 
-    glGenVertexArrays(2, VAO);
-    glGenBuffers(2, VBO);
+    glGenVertexArrays(3, VAO);
+    glGenBuffers(3, VBO);
+
+
+    //maze walls
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //winning tile
+    glBindVertexArray(VAO[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(winV), winV, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(winIn), winIn, GL_STATIC_DRAW); 
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // render loop
     // -----------
@@ -189,28 +226,14 @@ int main()
         // ------
     glClear(GL_COLOR_BUFFER_BIT);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    //player 
+    glBindVertexArray(VAO[2]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(playerCoords), playerCoords, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-
-
-    glBindVertexArray(VAO[1]);
-
-
-
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(winV), winV, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(winIn), winIn, GL_STATIC_DRAW); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[2]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(playerInd), playerInd, GL_STATIC_DRAW); 
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -223,14 +246,16 @@ int main()
     glBindVertexArray(0); 
 
 
-        // draw our first triangle
         glUseProgram(shaderProgram2);
         glBindVertexArray(VAO[0]); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-        glUseProgram(shaderProgram);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
         glUseProgram(shaderProgram2);
         glBindVertexArray(VAO[1]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO[2]);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glBindVertexArray(0); // no need to unbind it every time 
@@ -243,8 +268,8 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(2, VAO);
-    glDeleteBuffers(2, VBO);
+    glDeleteVertexArrays(3, VAO);
+    glDeleteBuffers(3, VBO);
     glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
@@ -254,29 +279,56 @@ int main()
 }
 
 
-bool collide(float obj1[], float obj2[], float dx, float dy){ //x1 x2 y1 y2
+bool collide(std::vector<float> obj1, std::set<std::vector<float>> walls, float dx, float dy){ //x1 x2 y1 y2
     obj1[0]+=dx;
     obj1[1]+=dx;
     obj1[2]+=dy;
     obj1[3]+=dy;
-    bool xl = (obj1[0] < obj2[0] && obj1[1] > obj2[0]) || (obj1[0] < obj2[1] && obj1[1] > obj2[1]) || (obj1[0] < obj2[0] && obj1[1] > obj2[1]) || (obj1[0] > obj2[0] && obj1[1] < obj2[1]);
-    bool yl = (obj1[2] < obj2[2] && obj1[3] > obj2[2]) || (obj1[2] < obj2[3] && obj1[3] > obj2[3]) || (obj1[2] < obj2[2] && obj1[3] > obj2[3]) || (obj1[2] > obj2[2] && obj1[3] < obj2[3]);
+    bool xl = false;
+    bool yl = false;
+    bool overlap = false;
+    for(const auto& obj2 : walls){
+        xl = ((obj1[0] < obj2[0] && obj1[1] > obj2[0]) || (obj1[0] < obj2[1] && obj1[1] > obj2[1]) || (obj1[0] < obj2[0] && obj1[1] > obj2[1]) || (obj1[0] > obj2[0] && obj1[1] < obj2[1]));
+        yl = ((obj1[2] < obj2[2] && obj1[3] > obj2[2]) || (obj1[2] < obj2[3] && obj1[3] > obj2[3]) || (obj1[2] < obj2[2] && obj1[3] > obj2[3]) || (obj1[2] > obj2[2] && obj1[3] < obj2[3]));
+        overlap = overlap || (xl && yl);
+    }
     obj1[0]-=dx;
     obj1[1]-=dx;
     obj1[2]-=dy;
     obj1[3]-=dy;
-    return xl&&yl;
+    return overlap;
 }
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-    float inc = 0.001f;
-    float curCord[] = {vertices[0], vertices[3], vertices[1], vertices[10]};
-    float mazeCord[] = {vertices[12], vertices[15], vertices[13], vertices[22]};
-    float winCoord[] = {-0.1f, 0.0f, -0.1f, 0.0f};
-
-    if(collide(curCord, winCoord, 0, 0)){
+    float inc = 0.01f;
+    std::vector<float> curCord;
+    curCord.push_back(playerCoords[0]);
+    curCord.push_back(playerCoords[3]);
+    curCord.push_back(playerCoords[1]);
+    curCord.push_back(playerCoords[10]);
+    std::set<std::vector<float>> walls;
+    std::vector<float> mazeCord;
+    mazeCord.push_back(vertices[0]);
+    mazeCord.push_back(vertices[3]);
+    mazeCord.push_back(vertices[1]);
+    mazeCord.push_back(vertices[10]);
+    std::vector<float> mazeCord2;
+    mazeCord2.push_back(vertices[12]);
+    mazeCord2.push_back(vertices[15]);
+    mazeCord2.push_back(vertices[13]);
+    mazeCord2.push_back(vertices[22]);
+    walls.insert(mazeCord);
+    walls.insert(mazeCord2);
+    std::vector<float> winCoord;
+    winCoord.push_back(winV[0]);
+    winCoord.push_back(winV[3]);
+    winCoord.push_back(winV[1]);
+    winCoord.push_back(winV[10]);
+    std::set<std::vector<float>> wins;
+    wins.insert(winCoord);
+    if(collide(curCord, wins, 0, 0)){
         float c1 = (rand()%10)/10.0f;
         float c2 = (rand()%10)/10.0f;
         float c3 = (rand()%10)/10.0f;
@@ -287,35 +339,35 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
-        if(vertices[3]+inc <= 1 && !collide(curCord, mazeCord, inc, 0)){
-            vertices[0]+=inc;
-            vertices[3]+=inc;
-            vertices[6]+=inc;
-            vertices[9]+=inc;
+        if(playerCoords[3]+inc <= 1 && !collide(curCord, walls, inc, 0)){
+            playerCoords[0]+=inc;
+            playerCoords[3]+=inc;
+            playerCoords[6]+=inc;
+            playerCoords[9]+=inc;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
-        if(vertices[0]-inc>=-1 && !collide(curCord, mazeCord, -inc, 0)){
-            vertices[0]-=inc;
-            vertices[3]-=inc;
-            vertices[6]-=inc;
-            vertices[9]-=inc;
+        if(playerCoords[0]-inc>=-1 && !collide(curCord, walls, -inc, 0)){
+            playerCoords[0]-=inc;
+            playerCoords[3]-=inc;
+            playerCoords[6]-=inc;
+            playerCoords[9]-=inc;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
-        if(vertices[1]-inc >= -1 && !collide(curCord, mazeCord, 0, -inc)){
-            vertices[1]-=inc;
-            vertices[4]-=inc;
-            vertices[7]-=inc;
-            vertices[10]-=inc;
+        if(playerCoords[1]-inc >= -1 && !collide(curCord, walls, 0, -inc)){
+            playerCoords[1]-=inc;
+            playerCoords[4]-=inc;
+            playerCoords[7]-=inc;
+            playerCoords[10]-=inc;
         }
     }
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
-        if(vertices[7]+inc <= 1 && !collide(curCord, mazeCord, 0, inc)){
-            vertices[1]+=inc;
-            vertices[4]+=inc;
-            vertices[7]+=inc;
-            vertices[10]+=inc;
+        if(playerCoords[7]+inc <= 1 && !collide(curCord, walls, 0, inc)){
+            playerCoords[1]+=inc;
+            playerCoords[4]+=inc;
+            playerCoords[7]+=inc;
+            playerCoords[10]+=inc;
         }
     }
 }
