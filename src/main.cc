8320638764ebb70370be09712c4b3b7aaac9d411
LOTLOUISCHO <louissc2@illinois.edu>
2021-11-16 11:32:16 -0600

    Tried to fix the speed but needs to be done later

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    /////////////////////////NEED TO CHANGE THE SPEED//////////////////////////
     Level level{0.01F, 0.025F, 0.025F, 0, 0.0F, 5, 5, {-0.6625F, 0.6625F}, {0.6625F, -0.6625F}, 10.0F};
     Level level2{0.01F, 0.025F, 0.025F, 0, 0.0F, 10, 10, {-0.6625F, 0.6625F}, {0.6625F, -0.6625F}, 10.0F};
     Game *game = new Game();
