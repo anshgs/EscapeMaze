4e5e77ebce1ada78965bea82520f9f0d97e82fbe
LOTLOUISCHO <louissc2@illinois.edu>
2021-11-17 17:15:53 -0600

    edited item

--- src/item.cc
+++ src/item.cc
@@ -1,7 +1,6 @@
 #include "item.hpp"
 
-item::item() : item_name_("") , speed_multiplier_(0) { //set the default energy item name to nothing and set the energy level to 0.
-
+item::item() : item_name_("") , speed_multiplier_(0) , invincible_(false) { //set the default energy item name to nothing and set the energy level to 0.
 }
 
 item::item(std::string item_name, double speed_multiplier, bool invincible) : item_name_(item_name) , speed_multiplier_(speed_multiplier), invincible_(invincible) { //constructor that sets the item name to the parameter's input name and the energy level to the parameter's energy level
