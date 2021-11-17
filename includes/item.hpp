b9c199ddf26bb8363279a5b4ad4b3785f62558a9
LOTLOUISCHO <louissc2@illinois.edu>
2021-11-17 13:00:37 -0600

    edited item

--- includes/item.hpp
+++ includes/item.hpp
@@ -2,14 +2,18 @@
 #define ITEM_HPP
 
 #include "user.hpp"
+#include <string>
 
 class item {
 
 public:
+    item();
+    item (std::string item_name, int energy_level);
     void Add_energy();
 
 private:
-    int energy_level;
+    std::string item_name_;
+    int energy_level_;
 };
 
 
--- src/item.cc
+++ src/item.cc
@@ -1,6 +1,19 @@
 #include "item.hpp"
 
-void item::Add_energy() {
-    energy_level++;
+item::item() : item_name_("") , energy_level_(0) { //set the default energy item name to nothing and set the energy level to 0.
+
+}
+
+item::item(std::string item_name, int energy_level) : item_name_(item_name) , energy_level_(energy_level) { //constructor that sets the item name to the parameter's input name and the energy level to the parameter's energy level
+
+}
+
+void item::Add_energy() { //add energy to the user
+    if (item_name_ == "Monster") {  ///Monster Drink
+        energy_level_ += 20;
+    } else if (item_name_ == "Coffee") {  ///Coffee Drink
+        energy_level_ += 10;
+    }
+    
 }
 
