40b6fc54746d047a94be35e5571a21373b2ba6ac
LOTLOUISCHO <louissc2@illinois.edu>
2021-11-17 19:20:47 -0600

    Item inside the master edited

--- includes/item.hpp
+++ includes/item.hpp
@@ -3,22 +3,33 @@
 
 #include "player.hpp"
 #include <string>
+#include <vector>
 
 using namespace std;
-class item {
-
-public:
-    item();
-    item (string item_name, double speed_multiplier, bool invincible);
-    void Add_energy();
+class Item {
 
 private:
+    float coord_x_ = 0;
+    float coord_y_ = 0;
+    float coord_z_ = 0;
+    float size_x_ = 0;
+    float size_y_ = 0;
     string item_name_;
-    double speed_multiplier_;
+    float speed_multiplier_;
     bool invincible_;
+    vector<string> possible_items_ = {"Monster" , "Coffee" , "Water"};
+
+public:
+    
+    float GetSizeX();
+    float GetSizeY();
+    float* GetHitbox();
+    void SetRandomAttributes();
+    float GetSpeedMultiplier();
+    bool GetInvincible();
+    
     
 };
-
 
 
 
--- src/item.cc
+++ src/item.cc
@@ -1,18 +1,65 @@
 #include "item.hpp"
 
-item::item() : item_name_("") , speed_multiplier_(0) , invincible_(false) { //set the default energy item name to nothing and set the energy level to 0.
-}
 
-item::item(std::string item_name, double speed_multiplier, bool invincible) : item_name_(item_name) , speed_multiplier_(speed_multiplier), invincible_(invincible) { //constructor that sets the item name to the parameter's input name and the energy level to the parameter's energy level
 
-}
+void Item::SetRandomAttributes() { //add energy to the user
+    int idx = rand()%possible_items_.size(); //random idx number generator from 0 to 2
+    item_name_ = possible_items_.at(idx);
+    
+    coord_x_ = -0.7f + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1.4f)));
+    coord_y_ = -0.7f + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1.4f)));
 
-void item::Add_energy() { //add energy to the user
-    if (item_name_ == "Monster") {  ///Monster Drink
-        speed_multiplier_ += 20;
+    if (item_name_ == "Monster") {  ///Monster Drink goes here
+        speed_multiplier_ = 2.5f;
+        size_x_ = 0.025f;
+        size_y_ = 0.025f;
     } else if (item_name_ == "Coffee") {  ///Coffee Drink
-        speed_multiplier_ += 10;
+        speed_multiplier_ += 2.0f;
+        size_x_ = 0.015f;
+        size_y_ = 0.015f;
+    } else if (item_name_ == "Water") {  ///Coffee Drink
+        speed_multiplier_ += 1.5f;
+        size_x_ = 0.01f;
+        size_y_ = 0.01f;
     }
     
+}
+
+float Item::GetSizeX(){
+    return size_x_;
+}
+float Item::GetSizeY(){
+    return size_y_;
+}
+
+
+float * Item::GetHitbox(){
+    float x1 = coord_x_ - size_x_/2.0F;
+    float x2 = coord_x_ + size_x_/2.0F;
+    float y1 = coord_y_ - size_y_/2.0F;
+    float y2 = coord_y_ + size_y_/2.0F;
+    //TODO: fix this
+    float * hitbox = new float[12];
+    hitbox[0] = x1;
+    hitbox[1] = y1;
+    hitbox[2] = 0.0F;
+    hitbox[3] = x2;
+    hitbox[4] = y1;
+    hitbox[5] = 0.0F;
+    hitbox[6] = x2;
+    hitbox[7] = y2;
+    hitbox[8] = 0.0F;
+    hitbox[9] = x1;
+    hitbox[10] = y2;
+    hitbox[11] = 0.0F;
+    return hitbox;
+}
+
+float Item::GetSpeedMultiplier() {
+    return speed_multiplier_;
+}
+    
+bool Item::GetInvincible() {
+    return invincible_;
 }
 
