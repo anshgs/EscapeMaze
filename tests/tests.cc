b7c2e857ca2f77c9b8dd52d9c4a4aac4af956cb4
Vickycth <42989065+Vickycth@users.noreply.github.com>
2021-12-06 04:14:52 -0600

    add test case of maze wall coordinates

--- tests/tests.cc
+++ tests/tests.cc
@@ -32,7 +32,7 @@ TEST_CASE( "Maze initialization" ) {
     REQUIRE(m->GetWidth() == 9);
 }
 
-TEST_CASE( "Wall generating" ) {
+TEST_CASE( "Maze::GenerateMaze Test" ) {
     Maze* m = new Maze(9, 9);
     m->GenerateMaze(9,9);
     //check the bounding wall is closed
@@ -50,14 +50,43 @@ TEST_CASE( "Wall generating" ) {
     }
 }
 
+TEST_CASE( "Maze::GetWallCoor Test h = 10"){
+    int count = 0;
+    //vertical
+    for(float i = 0.69f; i >-0.7f; i -= 0.14f) {
+        int countrow = 0;
+        //horizontal
+        for (float j = - 0.69f; j < 0.7f; j += 0.14f ){
+            REQUIRE(CastCoorFloatToGrid(j, i,10).first == countrow);
+            REQUIRE(CastCoorFloatToGrid(j, i,10).second == count);
+            countrow = countrow + 1;
+        }
+        count = count + 1;
+    }
+}
 
+TEST_CASE( "Maze::GetWallCoor Test h = 15"){
+    int count = 0;
+    //vertical
+    for(float i = 0.65f; i >-0.7f; i -= 0.0933f) {
+        int countrow = 0;
+        //horizontal
+        for (float j = - 0.65f; j < 0.7f; j += 0.0933f ){
+            REQUIRE(CastCoorFloatToGrid(j, i,15).first == countrow);
+            REQUIRE(CastCoorFloatToGrid(j, i,15).second == count);
+            countrow = countrow + 1;
+        }
+        count = count + 1;
+    }
+}
 
 TEST_CASE( "Background color of the maze changes" ) {
   
 }
 
 /////////////////////////////Test cases for the utils///////////////////////
-TEST_CASE("check coordinates casting") {
+TEST_CASE("CastCoorFloatToGrid test") {
+
 
 }
 
