fee051ed5ae2d2fb101941a1f8d14df6b9e9dd06
LOTLOUISCHO <louissc2@illinois.edu>
2021-12-05 21:49:25 -0600

    added templates for the test cases

---
+++ tests/tests.cc
@@ -0,0 +1,62 @@
+// clang-format on
+
+#ifndef CATCH_CONFIG_MAIN
+#  define CATCH_CONFIG_MAIN
+#endif
+
+#include "catch.hpp"
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                 Includes //
+/////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdexcept>
+
+#include "ai.hpp"
+#include "config.hpp"
+#include "game.hpp"
+#include "item.hpp"
+#include "locinfo.hpp"
+#include "maze.hpp"
+#include "player.hpp"
+#include "utils.hpp"
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                Test Cases //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////Test cases for the mazes////////////////////////
+TEST_CASE( "Maze initialization" ) {
+  
+}
+
+TEST_CASE( "Position of the walls in the Maze change" ) {
+  
+}
+
+TEST_CASE( "Background color of the maze changes" ) {
+  
+}
+
+/////////////////////////////Test cases for the levels///////////////////////
+
+TEST_CASE("Check for level change") {
+ 
+}
+
+
+/////////////////////////////Test cases for the players//////////////////////
+TEST_CASE("Player collides with the wall") {
+
+}
+
+TEST_CASE("Player collects the item") {
+  
+}
+
+
+TEST_CASE("Win the game") {
+ 
+}
+
+
