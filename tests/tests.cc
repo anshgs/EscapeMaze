22a698221d7a6e46e2ae22201794c5bde85b5b1e
LOTLOUISCHO <louissc2@illinois.edu>
2021-12-05 23:20:18 -0600

    completed player test cases

--- tests/tests.cc
+++ tests/tests.cc
@@ -38,25 +38,72 @@ TEST_CASE( "Background color of the maze changes" ) {
   
 }
 
-/////////////////////////////Test cases for the levels///////////////////////
+/////////////////////////////Test cases for the items///////////////////////
 
-TEST_CASE("Check for level change") {
+TEST_CASE("Set random attributes") {
  
+
 }
 
 
 /////////////////////////////Test cases for the players//////////////////////
-TEST_CASE("Player collides with the wall") {
+TEST_CASE("Checks the default constructor") {
+    Player p1 = new Player(); //Default constructor for this test case
+
+    REQUIRE(p1.GetSizeX() == 0); //checks if the x is 0 for the default constructor
+    REQUIRE(p1.GetSizeY() == 0); //checks if the y is 0 for the default constructor
+    REQUIRE(p1.GetSpeed() == 0); //checks if the default speed is 0 for the default constructor
+    
+    float coord_x = 2;
+    float coord_y = 2;
+    float speed = 5;
+    float size_x = 4;
+    float size_y = 4;
+
+    p1.SetAttributes(coord_x, coord_y, speed, size_x, size_y);
+
+    REQUIRE(p1.GetSizeX() == 4);
+    REQUIRE(p1.GetSizeY() == 4);
+    REQUIRE(p1.GetSpeed() == 5);
+
+    p1.SetSpeed(7);
+    REQUIRE(p1.GetSpeed() == 7);
+
+    p1.SetXCoord(3);
+    REQUIRE(p1.GetXCoord() == 3);
+
+    p1.SetYCoord(6);
+    REQUIRE(p1.GetYCoord() == 6);
+
+    float refresh_rate_ = 2;
+
+
+    p1.UpdateSpeed(refresh_rate_);
+    
+    REQUIRE(p1.GetSpeed() == 3.5);
+
+    float coord_x = 3;
+    float coord_y = 3;
+    float speed = 8;
+    float size_x = 5;
+    float size_y = 5;
+    p1.SetAttributes(coord_x, coord_y, speed, size_x, size_y);
+
+    p1.MoveUp();
+    REQUIRE(p1.GetYCoord() == 11);
+
+    p1.MoveDown();
+    REQUIRE(p1.GetYCoord() == -5);
+
+    p1.MoveLeft();
+    REQUIRE(p1.GetXCoord() == -5);
+
+    p1.MoveRight();
+    REQUIRE(p1.GetXCoord() == 11);
+    
 
 }
 
-TEST_CASE("Player collects the item") {
-  
-}
 
-
-TEST_CASE("Win the game") {
- 
-}
 
 
